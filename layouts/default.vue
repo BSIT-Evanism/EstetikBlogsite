7ed8e33b88ecb6d716d25536501f75521948459e
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-03-05 00:14:21 +0800

    anotha one

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@
                 
             
                         <aside class="flex group h-100vh b-0 b-r-1 border-solid bg-rose-200 w-44 flex-col hover:bg-rose-300 animation-all duration-300ms">
-                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:underline animate-all duration-700ms" to="/">Home</NuxtLink>
-                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:underline animate-all duration-700ms" to="/about">About</NuxtLink>
-                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:underline animate-all duration-700ms" to="/articles">Articles</NuxtLink>
-                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:underline animate-all duration-700ms" to="/people">People</NuxtLink>
+                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:translate-x-2 hover:before:content-['=>'] hover:duration-300ms animate-all duration-700ms" to="/">Home</NuxtLink>
+                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:translate-x-2 hover:before:content-['=>'] hover:duration-300ms animate-all duration-700ms" to="/about">About</NuxtLink>
+                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:translate-x-2 hover:before:content-['=>'] hover:duration-300ms animate-all duration-700ms" to="/articles">Articles</NuxtLink>
+                             <NuxtLink class="p4 h-2 w-20 m-5 fw700 no-underline text-slate-400 group-hover:text-slate-700 hover:translate-x-2 hover:before:content-['=>'] hover:duration-300ms animate-all duration-700ms" to="/people">People</NuxtLink>
                              <carousel class="translate-y-400px" />
                          </aside>
                      
